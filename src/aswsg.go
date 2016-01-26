e22e216a8b626a6b4e28212d4c6d17060119339d
Alexander Kulbartsch (rix) <alexander@kulbartsch.de>
2016-01-26 17:29:06 +0100

    organise source

--- src/aswsg.go
+++ src/aswsg.go
@@ -85,12 +85,66 @@ var paragraphTags = map[string]string{
 
 // var paragraphState string
 
-// simpleVar handling
+// general tool functions
 
 // WhiteSpaceTrim
 func WhiteSpaceTrim(in string) string {
 	return strings.Trim(in, " \t\n")
 }
+
+// counts the occurence of the first character of a string at the beginning,
+// and returns the first character, it's count and the string trimmed from
+// leading and ending whitespaces plus first character
+func firstCharCountAndTrim(line string) (firstChar string, count int, content string) {
+	if len(line) == 0 {
+		return "", 0, ""
+	}
+	firstChar = line[0:1]
+	for count = 1; line[count] == firstChar[0]; count++ {
+	}
+	content = strings.Trim(line[count:], " \t"+firstChar)
+	return firstChar, count, content
+}
+
+// ContainsOnly returns true if s contains only runes in the string only.
+// An empty string s is always true. If otherwise only is empty the result is false.
+func ContainsOnly(s, only string) bool {
+	var isin bool
+	if len(s) == 0 {
+		return true
+	}
+	if len(only) == 0 { // no none
+		return false
+	}
+	for _, c := range s {
+		isin = false
+		for _, m := range only {
+			if c == m {
+				isin = true
+			}
+		}
+		if isin == false {
+			return false
+		}
+	}
+	return true
+}
+
+// right returns the right most l char(s) of s in r
+func Right(s string, l int) (r string) {
+	le := len(s)
+	if le == 0 || l == 0 {
+		return ""
+	}
+	le -= l
+	if le < 0 {
+		le = 0
+	}
+	r = s[le:]
+	return
+}
+
+// simpleVar handling
 
 func (v SimpleVars) SetVar(key, val string) (ok bool) {
 	tkey := WhiteSpaceTrim(key)
@@ -173,54 +227,6 @@ func parseAndSetCommandLineVars() {
 	}
 }
 
-func firstCharCountAndTrim(line string) (firstChar string, count int, content string) {
-	if len(line) == 0 {
-		return "", 0, ""
-	}
-	firstChar = line[0:1]
-	for count = 1; line[count] == firstChar[0]; count++ {
-	}
-	content = strings.Trim(line[count:], " \t"+firstChar)
-	return firstChar, count, content
-}
-
-// ContainsOnly returns true if s contains only runes in the string only.
-// An empty string s is always true. If otherwise only is empty the result is false.
-func ContainsOnly(s, only string) bool {
-	var isin bool
-	if len(s) == 0 {
-		return true
-	}
-	if len(only) == 0 { // no none
-		return false
-	}
-	for _, c := range s {
-		isin = false
-		for _, m := range only {
-			if c == m {
-				isin = true
-			}
-		}
-		if isin == false {
-			return false
-		}
-	}
-	return true
-}
-
-// right returns the right most l char(s) of s in r
-func Right(s string, l int) (r string) {
-	le := len(s)
-	if le == 0 || l == 0 {
-		return ""
-	}
-	le -= l
-	if le < 0 {
-		le = 0
-	}
-	r = s[le:]
-	return
-}
 
 // inline
 
@@ -359,12 +365,15 @@ func parseCommonParagraphControls(line string, currentParagraphState string) (re
 	controlChars := siteVars.GetVal("ASWSG-LIST") + siteVars.GetVal("ASWSG-CITE") + siteVars.GetVal("ASWSG-NUMERATION")
 	// Message("", 0, "D", "inline:" + line)
 	// Message("", 0, "D", "  PS:" + currentParagraphState)
+
 	// parse raw
 	if firstChar == siteVars.GetVal("ASWSG-RAWLINE") {
 		resultingParagraphState = currentParagraphState
 		resultLines = append(resultLines, line[1:])
 		return
 	}
+
+	// parse LIST, CITE and NUMERATION
 	for _, r := range line {
 		a := string(r)
 		Message("", 0, "D", "  controlChar:"+a)
@@ -439,6 +448,10 @@ func parseLine(line string, paragraphState string) (resultLines []string, newPar
 		newParagraphState = parsedParagraph
 		return resultLines, newParagraphState
 	}
+
+	// parse commands
+	// todo
+
 
 	// TODO parse multi liner
 
